2630efd120f649af433903712c15c27de963f519
mersanuzun <mersanuzun@gmail.com>
2015-07-06 16:35:39 +0300

    Cell class' code

---
+++ Cell.java
@@ -0,0 +1,10 @@
+package battleships;
+
+public class Cell {
+    protected int placeCoorX;
+    protected int placeCoorY;
+    public Cell(int coorX, int coorY){
+        this.placeCoorX = coorX;
+        this.placeCoorY = coorY;
+    }
+}
