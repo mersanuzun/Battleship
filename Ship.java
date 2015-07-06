5281ff8dd743a629a916cd38a2647c9376649198
mersanuzun <mersanuzun@gmail.com>
2015-07-06 22:59:25 +0300

    a little bit modified for shooting method

--- Ship.java
+++ Ship.java
@@ -5,40 +5,32 @@ import java.util.* ;
 public class Ship {
     protected String name;
     protected int size;
-    protected Boolean sank;
-    ArrayList<Cell> placement = new ArrayList<Cell>();
+    protected Boolean sank = false;
+    ArrayList<Cell> shipCoordinates = new ArrayList<Cell>();
     public Ship(String name, int size){
         this.name = name;
         this.size = size;
     }
     public boolean shooting(int hitX, int hitY){
-        if (this.placement.isEmpty()){
-            this.sank = true;
-            return false;
+        boolean result = false;
+        if (this.shipCoordinates.isEmpty()){
+            result = false;
         }else{
-            for(int i = 0; i < this.placement.size(); i++){
-                if ((hitX == this.placement.get(i).placeCoorX) && 
-                    (hitY == this.placement.get(i).placeCoorY)){
-                       this.placement.remove(i);
+            for(int i = 0; i < this.shipCoordinates.size(); i++){
+                if ((hitX == this.shipCoordinates.get(i).placeCoorX) && 
+                    (hitY == this.shipCoordinates.get(i).placeCoorY)){
+                       this.shipCoordinates.remove(i);
+                       result = true;
+                        if (this.shipCoordinates.isEmpty()){
+                            this.sank = true;
+                        }
+                        break;
+                }else {
+                    result = false;
                 }
             }
+            
         }
-        return true;
-    }
-    public void placing(int coorX, int coorY, String direction){
-        switch(direction){
-            case "H" : 
-                for (int i = 0; i < this.size; i++){
-                    Cell c = new Cell(coorX + i, coorY);
-                    this.placement.add(c);
-                }
-                break;
-            case "V" : 
-                for (int i = 0; i < this.size; i++){
-                    Cell c = new Cell(coorX, coorY + i);
-                    this.placement.add(c);
-                }
-                break;
-        }  
+        return result;
     }
 }
